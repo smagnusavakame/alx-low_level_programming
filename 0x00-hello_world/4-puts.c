e04ec4191b350cddd2a8369ff40109901676d2be
selormmagnus <myemail@gmail.com>
2023-02-20 20:26:02 -0800

    my first line of c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.>
+/**
+ * main - a program to print a line using puts
+ *
+ * Return: 0
+**/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
